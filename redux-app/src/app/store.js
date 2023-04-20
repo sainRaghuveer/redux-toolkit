b393c711559ec7c649ee6b5690807d066153427d
sainRaghuveer <raghuveersain987@gmail.com>
2023-04-20 19:40:19 +0530

    todo

--- redux-app/src/app/store.js
+++ redux-app/src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import authReducer from "../features/Auth/AuthSlice"
+import authReducer from "../features/Auth/AuthSlice";
+import todoReducer from "../features/Todo/TodoSlice";
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
+    todo: todoReducer,
   },
 });
