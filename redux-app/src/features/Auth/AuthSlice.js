5472a0b1962e350751acc8de54ff574cd7445b87
sainRaghuveer <raghuveersain987@gmail.com>
2023-04-20 19:44:40 +0530

    todo

--- redux-app/src/features/Auth/AuthSlice.js
+++ redux-app/src/features/Auth/AuthSlice.js
@@ -55,9 +55,6 @@ export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 export const selectCount = (state) => state.counter.value;
 
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-
 export const incrementIfOdd = (amount) => (dispatch, getState) => {
   const currentValue = selectCount(getState());
   if (currentValue % 2 === 1) {
